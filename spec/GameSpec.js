b0dddfee5390a7a76d31a433af0a4269d54e8d4f
NowYouSeeMe5 <akcbryant@yahoo.com>
2016-07-12 19:59:59 -0700

    Set players according to input.

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -4,12 +4,32 @@ AiPlayer = require('../src/AiPlayer');
 UI = require('../src/UI');
 
 describe("setup", function() {
+
+  beforeEach(function() {
+    spyOn(Game, 'playGame');
+  });
+
   it("sets a human player as player 1 and an ai player as player 2 when the input is 1", function() {
-    spyOn(UI, "getNumberOfPlayers").and.returnValue(1);
+    spyOn(UI, 'getNumberOfPlayers').and.returnValue(0);
 
     Game.setup();
 
-    expect(HumanPlayer.move).toEqual(Game.player1);
-    expect(AiPlayer.move).toEqual(Game.player2);
-  })
-})
+    expect(Game.playGame).toHaveBeenCalledWith(AiPlayer.move, AiPlayer.move);
+  });
+
+  it("sets a human player as player 1 and an ai player as player 2 when the input is 1", function() {
+    spyOn(UI, 'getNumberOfPlayers').and.returnValue(1);
+
+    Game.setup();
+
+    expect(Game.playGame).toHaveBeenCalledWith(HumanPlayer.move, AiPlayer.move);
+  });
+
+  it("sets a human player as player 1 and an ai player as player 2 when the input is 1", function() {
+    spyOn(UI, 'getNumberOfPlayers').and.returnValue(2);
+
+    Game.setup();
+
+    expect(Game.playGame).toHaveBeenCalledWith(HumanPlayer.move, HumanPlayer.move);
+  });
+});
--- src/Game.js
+++ src/Game.js
@@ -4,17 +4,29 @@ AiPlayer = require('../src/AiPlayer');
 UI = require('../src/UI');
 
 var Game = {
-  player1: null,
-  player2: null,
-  board: null,
 
   setup: function() {
+    var that = this;
+
     var numberOfPlayers = UI.getNumberOfPlayers();
 
+    var player1Move = null;
+    var player2Move = null;
+
     if (numberOfPlayers == 1) {
-      this.player1 = HumanPlayer.move;
-      this.player2 = AiPlayer.move;
+      player1Move = HumanPlayer.move
+      player2Move = AiPlayer.move
+    } else if (numberOfPlayers == 2) {
+      player1Move = player2Move = HumanPlayer.move
+    } else {
+      player1Move = player2Move = AiPlayer.move
     }
+
+    this.playGame(player1Move, player2Move);
+  },
+
+  playGame: function(player1Move, player2Move) {
+
   }
 };
 
